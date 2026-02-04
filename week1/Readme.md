8d3ed35daaf06883e0d757338e89c99269764024
Howard <83273907+Negative-Newspaper@users.noreply.github.com>
2026-02-05 01:53:36 +0800

    Week 1 day 5 - daily expense tracker V2

---
+++ week1/Readme.md
@@ -0,0 +1,12 @@
+# daily expense tracker v1
+
+## A simple python program that tracks daily expense, calculate total and average, and check again the daily budget.
+
+## skills used
+- variables
+- input/output
+- conditions
+- for loops
+
+## how to run
+python daily_expenses_tracker.py
--- week1/daily_expenses_tracker.py
+++ week1/daily_expenses_tracker.py
@@ -10,16 +10,20 @@ days = int(input("Enter the number of days: "))
 budget = float(input("Enter you daily budget: "))
 
 expenses = 0
+over_the_budget = 0
 
 
 for day in range(days):
     expense = float(input(f"Day{day + 1} your expenses: "))
+    if expense >= budget:
+        over_the_budget += 1
     expenses += expense
     
 average = expenses / days
 
 print(f"Total spent: {expenses}")
 print(f"Average per day: {average}")
+print(f"Days over the budget: {over_the_budget}")
 
 if average > budget:
     print("Warning you are over the buget")
