39938d916e6b244d018841449fad1d97b9b588eb
Howard <83273907+Negative-Newspaper@users.noreply.github.com>
2026-03-31 02:02:40 +0800

    Transaction log V2 (added delete)

--- week1/W2_transaction_log_V2.py
+++ week1/W2_transaction_log_V2.py
@@ -6,6 +6,7 @@ today = datetime.datetime.today()
 file_exist = os.path.isfile("transaction.csv")
 
 def load_from_CSV_to_list(fileName):
+    expense_list = []
     if not os.path.exists(fileName):
             return []
     else:
@@ -20,10 +21,9 @@ def load_from_CSV_to_list(fileName):
                 except ValueError:
                     print(f"Skipping invalid value in row:{row}")
                     continue
+        return expense_list
         
 expense_list = load_from_CSV_to_list("transaction.csv")
-
-
 
 def add_expenses():
     expenses_to_add = int(input("How many expenses you will be adding? "))
@@ -34,24 +34,47 @@ def add_expenses():
         time = today.strftime("%Y-%m-%d %H:%M:%S")
         expenses = {"ID": 1, "Time": time, "Category": category, "Amount": amount }
         expense_list.append(expenses)
+    
+    save_transaction(expense_list)
 
 def save_transaction(expense_list):
-    keys = expense_list[0]
     
-    with open("transaction.csv", mode="a", newline="") as file:
-        write = csv.DictWriter(file, keys)
-        
-        if not file_exist:
-            write.writeheader()
-        
+    keyHeader = []
+    
+    if len(expense_list) > 0:
+        for dic in expense_list[0]:
+            keyHeader.append(dic)
+    else:
+        keyHeader = ["ID", "Time", "Category", "Amount"]
+           
+    with open("transaction.csv", mode="w", newline="") as file:
+        write = csv.DictWriter(file, keyHeader)   
+        write.writeheader()
         write.writerows(expense_list)
-        
 
-load_from_CSV_to_list("transaction.csv")
+def delete_transaction(expense_list):
+    Id_to_delete = int(input("Enter the ID that you want to delete: "))
+    lst_to_delete = []
+    
+    for expense in expense_list:
+        if expense["ID"] == Id_to_delete:
+            lst_to_delete.append(expense["ID"])
+            
+    
+    if len(lst_to_delete) > 0:
+        print("Id found")
+        for expense in reversed(expense_list):
+            if expense["ID"] == Id_to_delete:
+                expense_list.remove(expense) 
+                print(f"Succesfuly remove Id number: {Id_to_delete}")
+    else:
+        print("Data not found.")
+    
+            
+    save_transaction(expense_list)
+    print(lst_to_delete)
 
-print(expense_list)       
+     
 add_expenses()
-
-save_transaction(expense_list)
-
-print(expense_list)
+delete_transaction(expense_list)
+print(expense_list)   
